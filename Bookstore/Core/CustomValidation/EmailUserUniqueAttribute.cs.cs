c670bf2d0deb200591ef56fb5de0a7864871427f
Soteras <carlosoteras@gmail.com>
2021-04-14 23:54:23 -0300

    Email unique validation.

---
+++ Bookstore/Core/CustomValidation/EmailUserUniqueAttribute.cs.cs
@@ -0,0 +1,26 @@
+﻿using Bookstore.Infrastructure.Repository.Interface;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Core.CustomValidation
+{
+    public class EmailUserUniqueAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var repository = (IUserRepository)validationContext.GetService(typeof(IUserRepository));
+            var entity = repository.GetUserByEmail(value.ToString());
+
+            if (entity != null)
+            {
+                return new ValidationResult(GetErrorMessage(value.ToString()));
+            }
+
+            return ValidationResult.Success;
+        }
+
+        private string GetErrorMessage(string email)
+        {
+            return $"Email {email} já está em uso.";
+        }
+    }
+}
