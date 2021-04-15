fceb2bd5da82ee37a631ecfe7ef2809a3046742e
Soteras <carlosoteras@gmail.com>
2021-04-14 22:56:33 -0300

    Configure database.

---
+++ Bookstore/Infrastructure/Data/BookstoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Bookstore.Infrastructure.Data
+{
+    public class BookstoreContext: DbContext
+    {
+        public BookstoreContext(DbContextOptions<BookstoreContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
