a61fa34275b243690f995a85f933f6e104225f4c
Soteras <carlosoteras@gmail.com>
2021-04-15 00:01:04 -0300

    Signup controller.

--- Bookstore/Infrastructure/Repository/Interface/IUserRepository.cs
+++ Bookstore/Infrastructure/Repository/Interface/IUserRepository.cs
@@ -4,6 +4,7 @@ namespace Bookstore.Infrastructure.Repository.Interface
 {
     public interface IUserRepository
     {
+        public User GetUserByEmail(string email);
         public User Create(User user);
     }
 }
